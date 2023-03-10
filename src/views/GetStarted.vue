061052f1bb30d19c6eb1e059933759dede5bd311
Eric Miller <epmiller8464@users.noreply.github.com>
2023-03-10 12:35:50 -0600

    add toast

--- src/views/GetStarted.vue
+++ src/views/GetStarted.vue
@@ -1,6 +1,23 @@
 <template>
+  <div class="alert alert-light alert-dismissible fade show" role="alert">
+    This is an early, illustrative demo. Intent is to get initial reactions. <em>Lots more to come.</em>
+    <button type="button" class="btn-close alert-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+  <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
 
-  <div id="cta" class="container my-5 py-5">
+      <strong class="me-auto">Bootstrap</strong>
+      <small>11 mins ago</small>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+      Hello, world! This is a toast message.
+    </div>
+  </div>
+<!--  <div class="alert alert-dark" role="alert">-->
+<!--    A simple dark alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.-->
+<!--  </div>-->
+  <div id="cta" class="container py-5">
     <div class="row text-center">
       <h1>Learn what rebates & incentives you are eligible for.</h1>
       <p class=" fst-italic my-4">Up to $X,XXX incentives & rebates available today</p>
@@ -39,6 +56,17 @@ export default {
       this.surveyStore.loadCurrentSurvey()
       this.$router.push({ name: 'Survey', params: { index: 0 } })
     },
+  },
+  mounted () {
+    const toastTrigger = document.getElementById('liveToastBtn')
+    const toastLiveExample = document.getElementById('liveToast')
+    if (toastTrigger) {
+      toastTrigger.addEventListener('click', () => {
+        const toast = new bootstrap.Toast(toastLiveExample)
+
+        toast.hide()
+      })
+    }
   },
   setup () {},
 }
